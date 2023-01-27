9297e2b8af33c454265b1bc4df17b5d9034fb5e3
ThirumanikandanR <121092831+ThirumanikandanR@users.noreply.github.com>
2023-01-27 18:45:55 +0530

    erukea client servicve added successfully

--- ust_busbooking_admin/src/main/java/com/busbooking/admin/UstBusbookingAdminApplication.java
+++ ust_busbooking_admin/src/main/java/com/busbooking/admin/UstBusbookingAdminApplication.java
@@ -2,14 +2,13 @@ package com.busbooking.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(scanBasePackages = {"com.busbooking.admin","com.busbooking.data","com.busbooking.*"} )
 @PropertySources(@PropertySource("classpath:message.properties"))
+@EnableDiscoveryClient
 public class UstBusbookingAdminApplication {
 
 	public static void main(String[] args) {
