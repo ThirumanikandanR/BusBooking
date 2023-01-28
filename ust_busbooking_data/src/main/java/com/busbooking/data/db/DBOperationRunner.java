1c7ccabdbc393239b69c30c8c61f4037d3520f5b
ThirumanikandanR <121092831+ThirumanikandanR@users.noreply.github.com>
2023-01-28 14:27:39 +0530

    Initial DD configuration completed

--- ust_busbooking_data/src/main/java/com/busbooking/data/db/DBOperationRunner.java
+++ ust_busbooking_data/src/main/java/com/busbooking/data/db/DBOperationRunner.java
@@ -1,32 +1,37 @@
-//package com.busbooking.data.db;
-//
-//import java.util.Arrays;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.stereotype.Component;
-//
-//import com.busbooking.data.enums.ERole;
-//import com.busbooking.data.model.Role;
-//import com.busbooking.data.repository.RoleRepository;
-//
-//@Component
-//public class DBOperationRunner implements CommandLineRunner{
-//	
-//
-//    @Autowired
-//    RoleRepository roleRepository;
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//
-//    	roleRepository.saveAll(Arrays.asList(
-//
-//                     new Role("9ab2cb7e-7c27-11eb-9439-0242ac130002",ERole.ROLE_CUSTOMER),
-//                     new Role("9ab2cb7e-7c27-11eb-9439-0242ac130002",ERole.ROLE_ADMIN)
-//                    )
-//
-//           ); 
-//
-//}
-//}
+package com.busbooking.data.db;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.busbooking.data.enums.ERole;
+import com.busbooking.data.model.Role;
+import com.busbooking.data.repository.RoleRepository;
+
+@Component
+public class DBOperationRunner implements CommandLineRunner {
+
+	@Autowired
+	RoleRepository roleRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		if (roleRepository.count() == 0) {
+
+			long size = roleRepository.count();
+			System.out.println("Repo Size" + size);
+
+			roleRepository.saveAll(Arrays.asList(
+
+					new Role("9ab2cb7e-7c27-11eb-9439-0242ac130002", ERole.ROLE_CUSTOMER),
+					new Role("9ab2cb7e-7c27-11eb-9439-0242ac131234", ERole.ROLE_ADMIN))
+
+			);
+
+		}
+	}
+
+}
